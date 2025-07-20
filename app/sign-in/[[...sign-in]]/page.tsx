c3dd46fddaef7dead0369690fe4bc1464314c061
Kunal Garg <kunalgarg054@gmail.com>
2025-07-21 02:13:41 +0530

    refactor: ui background added on sign-in

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,8 +1,19 @@
 import { SignIn } from '@clerk/nextjs'
+import Image from 'next/image'
 
 export default function Page() {
   return (
     <div className="min-h-screen flex items-center justify-center">
+      <div className="absolute inset-0 z-0">
+        <Image
+          src="/new-bg.jpg"
+          alt="Background"
+          fill
+          className="object-cover"
+          priority
+        />
+        <div className="absolute inset-0 bg-black/20"></div>
+      </div>
       <SignIn />
     </div>
   )
